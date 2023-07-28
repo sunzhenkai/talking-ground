262a610bf0cae171b10ddee39888268b731cc901
zhenkai.sun <zhenkai.sun@qq.com>
2023-07-28 13:09:18 +0800

    add favors

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -17,20 +17,34 @@ export default defineConfig({
           {text: '机器学习', link: '/notes/artificial intelligence/machine learning'},
           {text: '深度学习', link: '/notes/artificial intelligence/deep learning'},
           {text: '大语言模型', link: '/notes/artificial intelligence/deep learning'},
-          {text: '应用', link: '/notes/artificial intelligence/applications'},
+          {text: '应用', items: [
+            {text: '推荐系统', link: ''}
+          ]},
         ]
       },
       {
         text: '收藏',
         items: [
           {text: '人工智能', link: '/notes/artificial intelligence/favor'},
-          {text: 'C++', link: '/notes/basic programming/c++/favor'}
+          {text: '机器学习', link: '/notes/artificial intelligence/applications/recommend system/favor'},
+          {text: '虚拟化', link: '/notes/computer science/virtualization technology/favor'},
+          {text: '编程基础', items: [
+            {text: 'C++', link: '/notes/basic programming/c++/favor'}
+          ]}
         ]
       },
       {
-        text: '链接',
+        text: '研习录',
         items: [
-          { text: '灵犀', link: 'https://exploring.fun' }
+          {text: '索引', link: '/notes/study record/list'}
+        ]
+      },
+      {
+        text: '外部链接',
+        items: [
+          { text: '灵犀', link: 'https://exploring.fun' },
+          { text: '博客', link: 'https://wii.pub' },
+          { text: 'github', link: 'https://github.com/sunzhenkai' }
         ]
       }
     ],
