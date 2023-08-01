4142a2a5f09d780e86d80d4cc9be7a1cbd22e359
zhenkai.sun <zhenkai.sun@qq.com>
2023-08-01 11:52:56 +0800

    mod menu

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,13 +12,100 @@ export default defineConfig({
 
     sidebar: [
       {
+        text: '我',
+        collapsed: true,
+        items: [
+          {
+            text: '关于'
+          },
+          {
+            text: '经历'
+          }
+        ]
+      },
+      {
+        text: '计算机科学',
+        items: [
+          {
+            text: '操作系统',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '网络',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '软件工程',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '编译原理',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '大数据',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '云计算',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '虚拟化',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '工程架构',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '前端技术',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '搜索引擎',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '密码学',
+            collapsed: true,
+            items: []
+          }
+        ]
+      },
+      {
         text: '编程基础',
         items: [
           {
-            text: 'C++',
+            text: '设计模式',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '数据结构',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '编程算法',
+            collapsed: true,
+            items: []
+          },
+          {
+            text: '语言',
             collapsed: true,
             items: [
-
+              {text: 'C++', link: '/notes/basic programming/c++/notes/basic'}
             ]
           }
         ]
@@ -80,7 +167,7 @@ export default defineConfig({
         ]
       },
       {
-        text: '外部链接',
+        text: '链接',
         items: [
           { text: '灵犀', link: 'https://exploring.fun' },
           { text: '博客', link: 'https://wii.pub' },
